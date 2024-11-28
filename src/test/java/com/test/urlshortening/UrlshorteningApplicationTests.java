5db4dbd140deda4287875c17d97c74ed327efa3f
tommy.mnan <tommymnan@gmail.com>
2024-11-28 22:20:32 +0800

    First commit

---
+++ src/test/java/com/test/urlshortening/UrlshorteningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.urlshortening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshorteningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
